09532388d24e68a2e016243d88d46cba33945709
MarcosAAlbanoJunior <marcosaalbanojunior@gmail.com>
2025-12-01 21:33:16 -0300

    first commit

---
+++ src/main/java/com/malbano/resilience4j/samples/Resilience4jSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.malbano.resilience4j.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4jSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Resilience4jSamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malbano/resilience4j/samples/Resilience4jSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malbano.resilience4j.samples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4jSamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
